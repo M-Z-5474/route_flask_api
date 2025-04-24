4538c315f2038c227fd5ae448b485fe36a263ae2
M-Z-5474 <195462331+M-Z-5474@users.noreply.github.com>
2025-04-24 11:55:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,7 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 400
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+       app.run(host="0.0.0.0", port=8080, debug=True)
+
